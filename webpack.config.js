c36fc8641fd9aa8448cc183f2b7c7ada7b1eaf4c
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-03-26 22:33:13 +0900

    [Add] Webpack Setting

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    hot: true,
+    host: 'localhost',
+    port: 8800,
+    open: true,
+  },
+};
