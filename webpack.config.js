6a94008f35c8ff0ac496516b6d094219d7465cbb
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-04-24 20:08:38 +0900

    [Modify] Build Config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 // const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 //ref: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
 module.exports = (env) => {
@@ -21,19 +22,14 @@ module.exports = (env) => {
         systemvars: true,
         path: `./.env.${env.development ? 'development' : 'production'}`,
       }),
-      // new webpack.EnvironmentPlugin({
-      //   'process.env': JSON.stringify(
-      //     dotenv.config({ path: `./.env.${mode}` }).parsed
-      //   ),
-      // }),
-      // new webpack.DefinePlugin({
-      //   'process.env': JSON.stringify(process.env),
-      // }),
-      // new webpack.ProvidePlugin({
-      //   'process.env': JSON.stringify(
-      //     dotenv.config({ path: `./.env.${mode}` }).parsed
-      //   ),
-      // }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/static/images'),
+            to: path.resolve(__dirname, 'dist/static/images'),
+          },
+        ],
+      }),
       new HtmlWebpackPlugin({
         hash: true,
         template: './src/index.html', //적용될 html 경로
@@ -66,18 +62,13 @@ module.exports = (env) => {
         directory: path.join(__dirname, 'src'),
       },
       historyApiFallback: true,
-      allowedHosts: 'all',
       compress: true,
-      // hot: true,
+      allowedHosts: 'all',
       host: 'localhost',
       port: 8800,
-      // open: true,
     },
     resolve: {
       fallback: {
-        // fs: false,
-        // path: false,
-        // process: false,
         buffer: require.resolve('buffer'),
       },
     },
