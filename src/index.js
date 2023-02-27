60dd4e015483f6156e687b55da9163c7100d0866
cloud <cloud@oysterable.com>
2023-02-27 19:04:32 +0900

    [Fix] Render again when nginx redirect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import App from './app.js';
 
 try {
+  const $root = document.querySelectorAll('#root');
+  $root.innerHTML = '';
+
   new App({
-    $target: document.querySelector('#root'),
+    $target: $root,
   });
 } catch (e) {
   alert(`인스턴스 에러발생 : ${e}`);
