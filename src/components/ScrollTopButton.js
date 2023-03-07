b736181d458612fa98218f974858ad28ea1ba382
cloud <cloud@oysterable.com>
2023-03-07 23:26:47 +0900

    [Refactor] ScrollTopButton Component as Class format

--- src/components/ScrollTopButton.js
+++ src/components/ScrollTopButton.js
@@ -1,21 +1,28 @@
-const ScrollTopButton = function ({ $target }) {
-  this.$target = $target;
-  const wrapper = document.createElement('button');
-  wrapper.setAttribute('class', 'scroll-top-button');
-  this.render = () => {
-    wrapper.innerHTML = `
+class ScrollTopButton {
+  constructor({ $target }) {
+    this.$target = $target;
+    this.$wrapper = document.createElement('button');
+    this.$wrapper.setAttribute('class', 'scroll-top-button');
+    $target.appendChild(this.$wrapper);
+
+    this.render();
+    this.addEventListeners();
+  }
+
+  render = () => {
+    this.$wrapper.innerHTML = `
     <i class="fas fa-arrow-up"></i>
     `;
-    this.$target.appendChild(wrapper);
   };
-  this.render();
 
-  wrapper.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
+  addEventListeners = () => {
+    this.$wrapper.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
     });
-  });
-};
+  };
+}
 
 export default ScrollTopButton;
