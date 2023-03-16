afb8f54a77c5688776cf991ea81300b80c69f506
cloud <cloud@oysterable.com>
2023-03-16 16:07:37 +0900

    [Modify] change detail list in resume

--- src/pages/Resume/constant.js
+++ src/pages/Resume/constant.js
@@ -105,14 +105,7 @@ export const EXPERIENCE = [
     coprEx: '개발팀 프론트엔드 개발자, <strong>프론트엔드 팀 리더</strong>',
     list: [
       {
-        title: '프론트엔드팀 리더 2022/11~',
-        detailList: [
-          'C Level, 백엔드 팀 리더 추천으로 팀 리더 지정',
-          '팀원 업무 일정 관리 및 분배',
-        ],
-      },
-      {
-        title: '배달음식 일회용기 반납 후 보증금 반환을 위한 서비스 개발',
+        title: '배달음식 일회용기 반납 후 보증금 반환을 위한 서비스',
         link: 'https://dish.lalaloop.app/',
         detailList: [
           '은행선택을 위한 Bottom Sheet',
@@ -121,7 +114,7 @@ export const EXPERIENCE = [
         ],
       },
       {
-        title: '사내 관리 시스템 공통으로 사용이 될 사내 UI라이브러리 개발',
+        title: '사내 관리 시스템 공통으로 사용이 될 사내 UI라이브러리',
         link: 'https://www.npmjs.com/package/@oysterable_public/oysterable-ui',
         detailList: [
           '백오피스에서 공통적으로 사용될 리액트 기반 UI 컴포넌트 및 공통 로직(Custom Hooks)을 NPM에 배포',
@@ -130,17 +123,17 @@ export const EXPERIENCE = [
         ],
       },
       {
-        title: 'IoT 기반 데이터 관리를 위한 사내 관리 시스템 개발',
+        title: 'IoT 기반 데이터 관리를 위한 사내 관리 시스템',
         link: 'https://oa-console.oysterable.com/',
         detailList: [
-          '기기 데이터 및 현장 자료 관제와 TCP서버로부터의 데이터 활용을 위한 백오피스',
+          '기기 데이터 및 현장 자료 관제와 TCP서버로부터의 데이터 활용을 위한 사내관리 시스템',
           'React, Hooks, ContextAPI, React-Query를 적극 활용하여 도메인, 클라이언트 상태 분리 개념 적용',
           '권한별 메뉴, 버튼 접근 등의 렌더링을 통한 다양한 UX 제공',
           'ApexChart를 활용하여 다양한 데이터를 시각화',
         ],
       },
       {
-        title: '오늘의 분리수거 앱 관리를 위한 사내 관리 시스템 개발',
+        title: '오늘의 분리수거 앱 관리를 위한 사내 관리 시스템',
         link: 'https://tr-console.oysterable.com/',
         detailList: [
           '오늘의 분리수거 앱의 데이터 및 유저 관리를 위한 백오피스 개발',
@@ -149,12 +142,20 @@ export const EXPERIENCE = [
         ],
       },
       {
-        title: '페트 및 캔 등의 반납 기능을 제공해주는 오늘의 분리수거 앱 개선',
+        title: '오늘의 분리수거 앱',
         link: '',
         detailList: [
+          '개발된 앱 내 웹뷰의 문제점 개선을 진행',
           '브라우저 캐시 활용 및 상태관리 개선을 통해 기존 대비 API호출 횟수 66% 감소',
           'Dynamic import 및 static파일 캐싱을 통해 LightHouse점수 퍼포먼스 50점 상승',
           '앱 내 웹뷰의 크로스 브라우저 대응을 위해 Polymer 활용',
+        ],
+      },
+      {
+        title: '프론트엔드팀 리더 [2022/11~ ]',
+        detailList: [
+          'C Level, 백엔드 팀 리더 추천으로 팀 리더 지정',
+          '팀원 업무 일정 관리 및 분배',
         ],
       },
       {
@@ -191,24 +192,18 @@ export const EXPERIENCE = [
       '디타임은 기업이 겪는 조직에 관한 다양한 어려움을 해결하는것을 목표로 교보생명의 엑셀러레이팅 프로그램에 선발된 스타트업입니다.',
     list: [
       {
-        title: 'Props drilling 개선 위한 패턴 변경',
+        title: '상태관리 패턴 변경',
         detailList: [
+          'Props drilling 개선 위한 패턴 변경',
           '뷰 ,로직 분리 위한 비동기 전역 상태관리 도입(Redux-Saga) 및 hooks 적극 활용',
           '비동기와 동기 상태 관리 일관성을 만들어 테스트의 용이성 상승',
         ],
       },
       {
-        title: '프론트 테스트 환경 구축',
+        title: '커스텀엘리먼트 개발',
         detailList: [
-          'Unit Test 위한 jest 도입',
-          'Business logic test를 위한 testing-library 활용',
-          'E2E 테스트를 위한 Cypress 도입',
-        ],
-      },
-      {
-        title: '협력업체 서비스 제공을 위해 커스텀엘리먼츠 개발',
-        detailList: [
-          '커스텀엘리먼츠 및 웹팩을 활용한 하나의 번들된 JavaScript파일을 협력업체에 제공',
+          '협력업체에 제공할 자사 서비스의 코드가 필요하여 개발 진행',
+          '커스텀엘리먼트 및 웹팩을 활용한 하나의 번들된 JavaScript파일을 협력업체에 제공',
         ],
       },
       {
@@ -225,8 +220,16 @@ export const EXPERIENCE = [
         link: '',
         detailList: [
           '앵귤러로 되어있던 메인페이지들의 디자인개선 작업과 동시에 리액트로 전환',
-          '30여가지 경우의수를 가진 휴가생성 케이스에 대해 UI/UX 및 테스트 코드진행',
+          '30여가지 경우의수를 가진 휴가생성 케이스에 대해 UI/UX 및 테스트 코드 작성',
           '달력 컴포넌트 커스터마이징',
+        ],
+      },
+      {
+        title: '프론트 테스트 환경 구축',
+        detailList: [
+          'Unit Test 위한 jest 도입',
+          'Business logic test를 위한 testing-library 활용',
+          'E2E 테스트를 위한 Cypress 도입',
         ],
       },
     ],
@@ -249,8 +252,11 @@ export const EXPERIENCE = [
       '빅데이터를 활용한 새벽배송 서비스를 제공하는 쇼핑 플랫폼 브랜디에서 프론트엔드 개발 인턴으로 참여하였습니다.',
     list: [
       {
-        title: 'Vue.js 기반으로 기존 쇼핑몰 사이트 클론 프로젝트 진행',
-        detailList: ['캐러셀, GNB, 제품리스트 렌더 등 다양한 UI/UX를 개발'],
+        title: '클론 프로젝트 진행',
+        detailList: [
+          'Vue.js 기반으로 기존 쇼핑몰 사이트 클론 개발 진행',
+          '캐러셀, GNB, 제품리스트 렌더 등 다양한 UI/UX를 개발',
+        ],
       },
     ],
     skill: ['Vue.js 2.xx', 'Vuex'],
@@ -270,31 +276,36 @@ export const PROJECT = [
         title: '컴포넌트 기반 개발',
         link: '',
         detailList: [
+          '재사용성 증대를 위해 Vanila js를 활용한 컴포넌트 기반 개발',
+          '상태기반 렌더링',
           'V1: 생성자 함수 기반 컴포넌트',
           'V2: 클래스 기반 컴포넌트',
-          '상태기반 렌더링',
         ],
       },
       {
-        title: '커스텀 라우팅 시스템을 통한 CSR 제공',
+        title: 'API 서버 개발',
         link: '',
         detailList: [
-          'V1: HashChange이벤트를 활용한 라우터',
-          'V2: History API를 활용한 라우터',
-        ],
-      },
-      {
-        title: 'Express.js를 사용하여 Markdown을 읽어오는 api 서버 개발',
-        link: '',
-        detailList: [
+          'Express.js를 사용하여 Markdown을 읽어오는 api 서버 개발',
           'highlight.js 라이브러리를 이용해 Markdown에서 Html로 변환',
           'Nginx의 포트 포워딩을 통해 하나의 EC2만 활용',
         ],
       },
       {
-        title: '웹팩을 활용한 빌드',
+        title: '라우팅 시스템',
         link: '',
-        detailList: ['Express.js를 제외한 프론트 코드만을 번들링'],
+        detailList: [
+          'Custom Router를 만들어 CSR과 유사한 라우터 개발',
+          'V1: HashChange이벤트를 활용한 라우터',
+          'V2: History API를 활용한 라우터',
+        ],
+      },
+      {
+        title: '번들링 시스템',
+        link: '',
+        detailList: [
+          '웹팩을 활용하여 최소한의 플러그인을 통해 개발서버와 빌드 시스템 구현',
+        ],
       },
       {
         title: 'Github link',
@@ -321,30 +332,42 @@ export const PROJECT = [
     ],
     list: [
       {
-        title:
-          '유기적인 패션커뮤니티 서비스 제공을 위해 기획, 디자인, 구현까지 1인 진행',
-        link: '',
-        detailList: [],
-      },
-      {
-        title: '팔로우 & 팔로잉 기능 구현',
+        title: '유저간 팔로우 팔로잉 제공',
         link: '',
         detailList: [
+          '팔로우 & 팔로잉 기능 구현',
           '크루, 유저, 브랜드 등 다양한 유저타입을 기반으로 상호 팔로잉 기능 제공',
         ],
       },
       {
-        title: '게시글, 댓글 CRUD 진행',
+        title: '게시물, 댓글 등록',
         link: '',
         detailList: [
-          '임시저장 및 불러오기 기능 구현',
+          '게시글, 댓글 CRUD 개발',
+          '작성 중 게시글 임시저장 및 불러오기 기능 구현',
           'ToastUI를 통한 다양한 글 작성 및 S3활용으로 이미지 등록 구현',
+          '라이브러리 커스터마이징을 통해 대표사진 등록 기능 제공',
+          '다양한 카테고리의 글 작성 기능 제공',
         ],
       },
       {
-        title: '이메일 인증시스템 구현',
+        title: '크루 생성 기능',
+        link: '',
+        detailList: [
+          '특정 등급의 유저는 개별적 크루 생성이 가능하게 구현',
+          '크루원으로 가입된 인원과 팔로우하는 인원의 글은 별도 탭에서 보여지게 구현',
+        ],
+      },
+      {
+        title: '이메일 인증시스템',
         link: '',
         detailList: ['Mailgun 라이브러리를 활용한 시스템 구현 '],
+      },
+      {
+        title:
+          '유기적인 패션커뮤니티 서비스 제공을 위해 기획, 디자인, 구현까지 1인 진행',
+        link: '',
+        detailList: [],
       },
       {
         title: 'FE Github link',
