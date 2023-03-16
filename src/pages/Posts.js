88c427a939cdf03b2d52951f260a3038a9937c0e
cloud <cloud@oysterable.com>
2023-03-16 14:02:38 +0900

    [Modify] Remove code for refresh test

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -152,12 +152,5 @@ export class Posts {
         addRouteEventListener(e);
       }
     });
-
-    window.addEventListener('beforeunload', (event) => {
-      // 표준에 따라 기본 동작 방지
-      event.preventDefault();
-      // Chrome에서는 returnValue 설정이 필요함
-      event.returnValue = '';
-    });
   };
 }
