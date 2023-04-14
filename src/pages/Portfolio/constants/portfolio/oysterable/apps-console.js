1bf8ea10b17abb492d6925d8c922bc5ed9f9f86e
cloud <cloud@oysterable.com>
2023-04-14 09:56:57 +0900

    [Fix] Typo in portfolio files

--- src/pages/Portfolio/constants/portfolio/oysterable/apps-console.js
+++ src/pages/Portfolio/constants/portfolio/oysterable/apps-console.js
@@ -35,21 +35,21 @@ export const APPS_CONSOLE = {
   ],
   exploreComment: `
     오늘의 분리수거 앱의 효율적 관리를 위한 내부관리시스템으로 시작된 프로젝트이며 
-    앱 내 제공되는 다양한 이벤트 및 배너, 공지사항 등의 수정 및 작성을 위한 기능이 제공되며 내부 임직원의 업무효율 향상을 도모.
+    앱 내 제공되는 다양한 이벤트 및 배너, 공지사항 등의 수정 및 작성을 위한 기능이 제공되며 내부 임직원의 업무효율 향상을 도모했습니다.
   `,
   devExplains: [
     {
       image:
         '../../../../../../../static/images/portfolio/oysterable/apps-console/apps-console-shopping-guide.png',
       explanation: `
-      React-Query를 사용하여 도메인상태와 UI상태 분리를 진행하여 불 필요한 메모리 낭비를 지양
+      React-Query를 사용하여 도메인상태와 UI상태 분리를 진행하여 불 필요한 메모리 낭비를 지양하였습니다.
       `,
     },
     {
       image:
         '../../../../../../../static/images/portfolio/oysterable/apps-console/react-query.jpeg',
       explanation: `
-      중복 UI 및 로직은 각각 UI라이브러리 및 Hooks로 분리하여 재활용을 진행하며 중복되는 코드의 작성을 감소
+      중복 UI 및 로직은 각각 UI라이브러리 및 Hooks로 분리하여 재활용을 진행하며 중복되는 코드의 작성을 감소시켰습니다.
       `,
     },
   ],
--- src/pages/Portfolio/constants/portfolio/oysterable/today-recycle.js
+++ src/pages/Portfolio/constants/portfolio/oysterable/today-recycle.js
@@ -28,7 +28,7 @@ export const TODAY_RECYCLE = {
   techEnvironment: ['TypeScript'],
   libs: ['React.js', 'redux-toolkit', 'styled-components'],
   exploreComment: `  
-  오늘의 분리수거 앱은 캔, 투명 페트, 종이팩등의 반납기기가 제공되며 분리배출을 실천 시 포인트 제공 및 기업, 지역 등에서 제공해 준 다양한 보상품을 구매할 수 있는 서비스를 제공
+  오늘의 분리수거 앱은 캔, 투명 페트, 종이팩등의 반납기기가 제공되며 분리배출을 실천 시 포인트 제공 및 기업, 지역 등에서 제공해 준 다양한 보상품을 구매할 수 있는 서비스를 제공합니다.
   `,
   devExplains: [
     {
@@ -36,14 +36,14 @@ export const TODAY_RECYCLE = {
         '../../../../../../static/images/portfolio/oysterable/today-recycle/tr-map.png',
       explanation: `
         자주 변경이 일어나는 API의 캐시를 제공 및 적절한 상태관리 패턴으로의 변경을 통해 기존에 무분별하게 호출되던 토큰 변경 API, 유저정보 호출, 포인트 정보 호출등의 API를
-        50회(라우팅 기준의 유저플로우)에서 6회(로그인,구매, 기부 등의 일부 시점)로 90% 정도 감소, 이로 인해 서버자원의 낭비방지 및 UX 개선
+        50회(라우팅 기준의 유저플로우)에서 6회(로그인,구매, 기부 등의 일부 시점)로 90% 정도 감소, 이로 인해 서버자원의 낭비방지 및 UX 개선을 진행했습니다.
       `,
     },
     {
       image:
         '../../../../../../static/images/portfolio/oysterable/today-recycle/polyfill.png',
       explanation: `
-      비즈니스의 주 고객이 구 버전의 Android기기 사용이 다수 있어 웹뷰 내부의 일부 메서드(Array의 some, every 등)가 지원되지 않아 기능의 누락이 되는 것을 발견, react-app-polyfill의 일부 항목을 적용시켜 이를 해결
+      비즈니스의 주 고객이 구 버전의 Android기기 사용이 다수 있어 웹뷰 내부의 일부 메서드(Array의 some, every 등)가 지원되지 않아 기능의 누락이 되는 것을 발견, react-app-polyfill의 일부 항목을 적용시켜 이를 해결하였습니다.
       `,
     },
   ],
