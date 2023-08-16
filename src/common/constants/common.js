3fb5da339cfbb76093c4591d97c063a06d52ae04
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-08-16 16:31:46 +0900

    [Add] 객체지향의 사실과 오해 Chapter 2 (#134)
    - 이상한 나라의 객체
    
    Co-authored-by: cloud <cloud@oysterable.com>

--- src/common/constants/common.js
+++ src/common/constants/common.js
@@ -1,4 +1,5 @@
 export const POST_SELECT_MAP = [
+  // { name: 'All', key: 'all' },
   { name: 'Dev', key: 'post-dev' },
   { name: 'Solving', key: 'post-algorithm' },
   { name: 'Personnel', key: 'post-personnel' },
