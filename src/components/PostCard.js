4f37552b43beab23e3240c9fb4e249410d570914
sanghunlee-711 <cloudlee711@gmail.com>
2024-01-05 13:11:36 +0900

    [#151] Improve UX about clicking post not only title but also whole card UI

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,4 +1,4 @@
-import { setPreview } from '../common/utils/index.js';
+import { setPreview } from "../common/utils/index.js";
 
 export const PostCard = ({
   imgUrl,
@@ -11,30 +11,32 @@ export const PostCard = ({
   content,
 }) => {
   return `
-  <article class="each_post_container">
-    <div class="each_post_contents">
-      <h1>
-        <a href="/content?id=${slug}&type=${sortKey}" data-id="route">${title}</a>
-      </h1>
-      <div class="title_image" style="background-image:url(${imgUrl})"></div>
-      <div class="each_post_profile">
-        <img src="../../static/images/profile/selfie_japan.jpeg" alt="profile_image" loading="lazy">
-      <div class ="each_post_profile_detail">
-        <span>Cloud Lee</span>
-        <div>
-          <div class="post_category_$wrapper">
-            ${categories?.map((category) => `<span>${category}</span>`)}
+  <a href="/content?id=${slug}&type=${sortKey}" data-id="route">
+    <article class="each_post_container">
+      <div class="each_post_contents">
+        <h1>
+          ${title}
+        </h1>
+        <div class="title_image" style="background-image:url(${imgUrl})"></div>
+        <div class="each_post_profile">
+          <img src="../../static/images/profile/selfie_japan.jpeg" alt="profile_image" loading="lazy">
+        <div class ="each_post_profile_detail">
+          <span>Cloud Lee</span>
+          <div>
+            <div class="post_category_$wrapper">
+              ${categories?.map((category) => `<span>${category}</span>`)}
+            </div>
+            <span class="each_post_profile_detail_date">${
+              date?.split(" ")[0]
+            }</span>
           </div>
-          <span class="each_post_profile_detail_date">${
-            date?.split(' ')[0]
-          }</span>
+        </div>
+        </div>
+        <div class="preview_content">
+            ${summary ? summary : setPreview(JSON.parse(content))}
         </div>
       </div>
-      </div>
-      <div class="preview_content">
-          ${summary ? summary : setPreview(JSON.parse(content))}
-      </div>
-    </div>
-  </article>
+    </article>
+  </a>
   `;
 };
