d8087d9874fa690d5b40a05fad77d982f5c83e12
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2026-02-04 10:25:36 +0900

    Remove hardcoded GA ID fallback

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import App from "./app.js";
 
-import { makeGA } from "./common/utils/ga.js";
-
 try {
   const $root = document.querySelector("#root");
   $root.innerHTML = "";
@@ -9,8 +7,6 @@ try {
   new App({
     $target: $root,
   });
-
-  if (process.env.NODE_ENV === "production") makeGA();
 } catch (e) {
   alert(`인스턴스 에러발생 : ${e}`);
   console.error(e);
